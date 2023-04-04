6ac0f07a24fc004cb47906093399a891f53bd37f
ravi kant chauhan <ravikantchauhan65@gmail.com>
2023-04-04 19:00:27 +0530

    background of home fixed

--- frontend/src/CheckPage.js
+++ frontend/src/CheckPage.js
@@ -1,40 +1,45 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import { Button, TextField, Link } from "@material-ui/core";
 import "./checkPage.css";
-const imgMyimageexample = require("./nastuh-abootalebi-yWwob8kwOCk-unsplash.jpg");
-const divStyle = {
-  backgroundImage: `url(${imgMyimageexample})`,
-        backgroundPosition: 'center',
-        backgroundSize: 'cover',
-        backgroundRepeat: 'no-repeat',
-        width: '100vw',
-        height: '55vh',
-        
-};
+// const imgMyimageexample = require("./nastuh-abootalebi-yWwob8kwOCk-unsplash.jpg");
+// const divStyle = {
+//   backgroundImage: `url(${imgMyimageexample})`,
+//         // backgroundPosition: 'center',
+//         backgroundSize: 'cover',
+//         backgroundRepeat: 'no-repeat',
+//         width: '100vw',
+//         height: '100vh',
+// };
 
 export default class CheckPage extends Component {
   render() {
     return (
-      <div className='startingPage'style={divStyle}>
-        
-         <Button
+      <div className="startingPage">
+        <div className="contentname">
+          <Button
             className="button_style"
-            variant="contained"
+            variant="contained" 
+            disableElevation
             color="primary"
-            size="small"
+            size="large"
+            
           >
-         <Link href="/userLogin"><span style={{color:"white"}}>User Login</span></Link>
+            <Link href="/userLogin">
+              <span style={{ color: "white" }}>User Login</span>
+            </Link>
           </Button>
           <Button
             className="button_style"
             variant="contained"
             color="primary"
-            size="small"
+            size="large"
           >
-          <Link href="/log"><span style={{color:"white"}}>Admin Login</span></Link>
-          </Button>     
-
+            <Link href="/log">
+              <span style={{ color: "white" }}>Admin Login</span>
+            </Link>
+          </Button>
+        </div>
       </div>
-    )
+    );
   }
 }
